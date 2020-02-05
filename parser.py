130d761dc06a6463c8049e5af1fa23410739eed7
Eric <ericpaul105@gmail.com>
2020-02-05 16:54:57 +0530

    Changes
    child tag name change code

--- parser.py
+++ parser.py
@@ -4,8 +4,8 @@ tree = ET.parse('/home/eric/IITB/Xcos-Converter/xcos_converter/example.xml')
 root = tree.getroot()
 
 for child in root:
-    child.tag ='hey'
+    child.tag ='test1'
 
     print(child.tag,child.attrib)
 
-tree.write('/home/eric/Documents/new.xml')
+tree.write('/home/eric/IITB/Xcos-Converter/xcos_converter/new.xml')
