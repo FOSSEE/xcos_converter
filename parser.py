0e40ccba04d5c1b16ecb29e2aacff7bb795d4c50
Eric <ericpaul105@gmail.com>
2020-02-06 11:55:32 +0530

    Selecting child nodes completed

--- parser.py
+++ parser.py
@@ -1,11 +1,32 @@
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
-tree = ET.parse('/home/eric/IITB/Xcos-Converter/xcos_converter/example.xml')
+tree = ET.parse('/home/eric/IITB/xcos_converter/example.xml')
+data = minidom.parse('/home/eric/IITB/xcos_converter/example.xml')
 root = tree.getroot()
+iter = root.getiterator()
+
+
+''' for elem in iter:
+
+    if elem.keys():
+        for name,value in elem.items():
+            if value == 'Liechtenstein':
+                value = 'Test3'
+                print(value) '''
+            
+
+
 
 for child in root:
-    child.tag ='test1'
+    if child.tag == 'USA':
+        child.tag ='Xml'
+    for elem in iter:
+        if elem.keys():
+            for name,value in elem.items():
+                if value == 'Singapore':
+                    value = 'Test3'
+                print(value)
 
-    print(child.tag,child.attrib)
 
-tree.write('/home/eric/IITB/Xcos-Converter/xcos_converter/new.xml')
+tree.write('/home/eric/IITB/xcos_converter/new.xml')
