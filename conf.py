71824cd834eb90b6c74a6fb825b8c5e0c58f421d
Eric <ericpaul105@gmail.com>
2020-02-12 15:40:02 +0530

    code for changing nodes based on names and accessing gchild nodes

--- conf.py
+++ conf.py
@@ -2,3 +2,7 @@
 rules = {"New":"USA","China":"Test1"}
 
 r1={"Singapore":"Asia","Chile":"S.America"}
+
+gchild={"New York":{"rank":"test2"}}
+
+ls=["Chile", "Singapore"]
--- parser.py
+++ parser.py
@@ -1,34 +1,41 @@
 import xml.etree.ElementTree as ET
-from xml.dom import minidom
-
 import conf
 #/home/eric/IITB/xcos_converter/
 tree = ET.parse('example.xml')
-data = minidom.parse('example.xml')
 root = tree.getroot()
 iter = root.getiterator()
+ch=root.getchildren()
 
+l=[]
 
 for child in root:
+    #remove parent tag by searching attrib
+    for n,v in child.items():
+        l.append(v)
+        '''if v == 'Singapore':
+            root.remove(child)'''
+
+    if child.attrib == 'name':
+        root.remove(child)
+
     #change node based on name
-    for a in conf.rules:
+    '''for a in conf.rules:
         if child.tag in conf.rules:
-            child.tag = conf.rules[child.tag]
+            child.tag = conf.rules[child.tag]'''
 
-    #accessing grandchild nodes
-    '''for gchild in child:
-        if gchild.tag == 'rank':
-            print(gchild.text)'''       
+    #changing gchild nodes based on child attrib
+    '''for n,v in child.items():
+        if v == 'Singapore':
+            for r in child:
+                if r.tag=='rank':
+                    r.tag="test2"'''
+            
 
-#changing  nodes based on attributes
-    for elem in iter:
+    #changing  nodes based on attributes
+    '''for elem in iter:
         if elem.keys():
             for name,value in elem.items():
                 if value in conf.r1:
-                    elem.tag=conf.r1[value]
-                '''elif value == 'Singapore':
-                    elem.tag='Asia' '''
-                #print(value)
-
+                    elem.tag=conf.r1[value]'''
 
 tree.write('new.xml')
