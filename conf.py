716b9c1ac8ff4e326d288efc6e871b47acd476a5
Eric <ericpaul105@gmail.com>
2020-03-05 16:44:51 +0530

    new version of conf file

--- conf.py
+++ conf.py
@@ -1,68 +1,223 @@
-path = [
-    {
-        'tag': 'ScilabDouble',
-        'attr': 'as',
-        'attrvalue': 'integerParameters'
-    },
+DOUBLE_TO_INTEGER = 1
+CHANGE_ATTRIB = 2
+DELETE_ATTRIB = 3
+MAIN_BLOCK = 4
+ADD_SUB_SUBTAG = 5
+DELETE_SUBTAG = 6
+DOUBLE_TO_INTEGER_AND_SWAP = 7
+
+path = []
+rule = []
+
+
+path.append(
     {
         'tag': 'ScilabDouble',
         'attr': 'as',
         'attrvalue': 'nmode'
-    },
+    })
+rule.append({
+    DOUBLE_TO_INTEGER:
+    {'tag':'ScilabInteger',
+    'attribute':{'intPrecision':'sci_int32'}}
+})
+
+path.append(
+    {
+        'tag': 'ScilabDouble',
+        'subtag':'data',
+        'attr':'realPart'
+    })
+rule.append({
+    CHANGE_ATTRIB:
+    {'attr':'value'}
+})
+
+path.append(    
     {
         'tag': 'ScilabDouble',
         'attr': 'as',
         'attrvalue': 'nbZerosCrossing'
+    })
+rule.append({
+    DOUBLE_TO_INTEGER:
+    {'tag':'ScilabInteger',
+    'attribute':{'intPrecision':'sci_int32'}
     }
-]
+})
 
-exp = [
-        
-        {
-            'tag' : 'ExplicitOutPutPort'
-        },
-        {
-            'tag' : 'ExplicitInputPort'
-        }
-        
-]
-replacement = [
-
-        {
-            'tag': 'ScilabDouble',
-            'attr': 'as',
-            'attrvalue': 'integerParameters'
-        }
-
-]
-#rule for changing node based on name
-attr=['nmode','nbZerosCrossing','integerParameters']
-rules = {"ScilabDouble":"ScilabInteger"}
-
-#setting new attrib
-at=['nmode','nbZerosCrossing','integerParameters']
-a=["intPrecision"]
-b=["sci_int32"]
-
-#removing nodes
-ls=[]
-
-#changing existing attribute values
-lst = ['realPart']
-t = ['value']
+path.append(
+    {
+        'tag': 'ScilabDouble',
+        'subtag':'data',
+        'attr':'realPart'
+    })
+rule.append({
+    CHANGE_ATTRIB:
+    {'attr':'value'}
+})
 
 
+path.append(
+    {
+        'tag':'BasicBlock',
+        'attr':'ordering',
+    })
+rule.append({
+    DELETE_ATTRIB:
+    {'attr':'ordering'}
+})
+rule.append({
+    MAIN_BLOCK:
+    {'tag':'ScilabDouble',
+    'attr':{'as':'state','height':'0','width':'0'}
+    }
+})
+rule.append({
+    MAIN_BLOCK:
+    {'tag':'ScilabDouble',
+    'attr':{'as':'dState','height':'0','width':'0'}
+    }
+})
 
-#to do
-tags  = []
+path.append(
+    {
+        'tag':'Product',
+        'attr':'ordering',
+    })
+rule.append({
+    'op':DELETE_ATTRIB,
+    'attr':'ordering'
+})
+rule.append({
+    MAIN_BLOCK:
+    {'tag':'ScilabDouble',
+    'attr':{'as':'state','height':'0','width':'0'}
+    }
+})
+rule.append({
+    MAIN_BLOCK:
+    {'tag':'ScilabDouble',
+    'attr':{'as':'dState','height':'0','width':'0'}}
+})
 
-'''attrs = {'as':'exprs'}
-newvalue = 'newvalue'
-av = {attrs:newvalue}
-tags.append({'ScilabString':av})
 
-attrs = {'height':'1'}
-newvalue = '2'
-av = {attrs:newvalue}
-tags.append({'ScilabDouble':av})'''
+path.append(
+    {
+        'tag':'BigSom',
+        'attr':'ordering',
+    })
+rule.append({
+    DELETE_ATTRIB:
+    {'attr':'ordering'}
+})
+rule.append({
+    MAIN_BLOCK:
+    {'tag':'ScilabDouble',
+    'attr':{'as':'state','height':'0','width':'0'}}
+})
+rule.append({
+    MAIN_BLOCK:
+    {'tag':'ScilabDouble',
+    'attr':{'as':'dState','height':'0','width':'0'}}
+})
 
+
+path.append(
+    {
+        'tag':'AfficheBlock',
+        'attr':'ordering',
+    }
+)
+rule.append({
+    DELETE_ATTRIB:
+    {'attr':'ordering'}
+})
+rule.append({
+    MAIN_BLOCK:
+    {'tag':'ScilabDouble',
+    'attr':{'as':'state','height':'0','width':'0'}}
+})
+rule.append({
+    MAIN_BLOCK:
+    {'tag':'ScilabDouble',
+    'attr':{'as':'dState','height':'0','width':'0'}}
+})
+
+path.append(
+    {
+        'tag':'ExplicitLink',
+        'subtag':'mxGeometry',
+        'attr':'y',
+    }
+)
+rule.append({
+    DELETE_ATTRIB:
+    {'attr':'y'}
+})
+rule.append({
+    ADD_SUB_SUBTAG:
+    {'subsubtag':'mxGeometry',
+    'attr':{'as':'sourcePoint','x':'0.0','y':'0.0'}}
+})
+rule.append({
+    ADD_SUB_SUBTAG:
+    {'subsubtag':'mxGeometry',
+    'attr':{'as':'targetPoint','x':'0.0','y':'0.0'}}
+})
+
+path.append(
+    {
+        'tag':'ExplicitLink',
+        'subtag':'mxGeometry',
+        'subsubtag':'Array',
+        'attr':'scilabClass',
+    }
+)
+rule.append({
+    DELETE_ATTRIB:
+    {'attr':'scilabClass'}
+})
+
+path.append(
+    {
+        'tag':'ExplicitInputPort',
+        'subtag':'mxGeometry' ,
+    })
+rule.append({
+    DELETE_SUBTAG:
+    {'subtag':'mxGeometry'}
+})
+
+path.append(
+    {
+        'tag':'ExplicitOutputPort',
+        'subtag':'mxGeometry' ,
+    })
+rule.append({
+    DELETE_SUBTAG:
+    {'subtag':'mxGeometry'}
+})
+
+path.append(
+    {
+        'tag': 'ScilabDouble',
+        'attr': 'as',
+        'attrvalue': 'integerParameters'
+    })
+rule.append({
+    DOUBLE_TO_INTEGER_AND_SWAP:
+    {'tag':'ScilabInteger',  
+    'attribute':{'intPrecision':'sci_int32'}}
+})
+
+path.append(
+    {
+        'tag': 'ScilabDouble',
+        'subtag':'data',
+        'attr':'realPart'
+    })
+rule.append({
+    CHANGE_ATTRIB:
+    {'attr':'value'}
+})
--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 #from xml.etree.ElementTree import Element
-from xml.etree.ElementTree import Element , SubElement
+from xml.etree.ElementTree import Element , SubElement,Comment
 from xml.etree.ElementTree import ElementTree
 import conf
 #/home/eric/IITB/xcos_converter/
@@ -10,19 +10,27 @@ iter = root.getiterator()
 ch=root.getchildren()
 
 for i, path in enumerate(conf.path):
-    xpath = './/' + path['tag'] + '[@' + path['attr'] + '=\'' + path['attrvalue'] + '\']'
+    xpath = './/' + path['tag'] 
+    #if path.get('attr', None) is not None:
+        #path.get('attrvalue', None) is not None
+    if 'attr' in path:
+        xpath += '[@' + path['attr']
+        if 'attrvalue' in path:
+            xpath += '=\'' + path['attrvalue'] + '\''
+        xpath += ']'    
+    
     #print(xpath)
     nodes = root.findall(xpath)
-    #nd = root.find(xpath)
+    rl = conf.rule[i]
     for node in nodes:
-        if node.tag == path['tag']:
-            node.tag="ScilabInteger"
-        for n in node:
-            for a,b in n.items():
-                if a == 'realPart': 
-                    b=str(int(float(b)))
-                    n.attrib[a] = b
-                    n.attrib['value'] = n.attrib[a]
-                    del n.attrib[a]
+        for n,v in rl.items():
+            if n == 1 :
+                node.tag = v['tag']
+                print(v['attribute'])
 
+            if n == 3:
+                print(v['attr'])
+                del node.attrib[v['attr']]
 tree.write('new.xml')
+
+# Author - Eric Paul
