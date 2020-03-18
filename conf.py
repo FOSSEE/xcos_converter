88ca76399655fe1d2a6bfe639840093c36b2bc88
Eric <ericpaul105@gmail.com>
2020-03-18 10:05:26 +0530

    Rewriting conf file, dictionary keys replaced with keywords

--- conf.py
+++ conf.py
@@ -7,304 +7,321 @@ DOUBLE_TO_INTEGER_AND_SWAP = 6
 DELETE_SUB_ATTRIB = 7
 DELETE_SUBSUB_ATTRIB = 8
 ADD_ATTRIB = 9
+KEY_RULE_OP = 'op'
+KEY_RULE_TAG = 'tag'
+KEY_RULE_ATTRIBUTE = 'attribute'
+KEY_RULE_ATTR = 'attr'
+KEY_RULE_VALUE = 'value'
+KEY_RULE_ATTR1 = 'attr1'
+KEY_RULE_SUBTAG = 'subtag'
+KEY_RULE_SUBSUBTAG = 'subsubtag'
+KEY_RULE_SUBSUBTAG1 = 'subsubtag1'
+KEY_PATH_TAG = 'tag'
+KEY_PATH_ATTR = 'attr'
+KEY_PATH_ATTRVALUE = 'attrvalue'
+KEY_PATH_SUBTAG = 'subtag'
+KEY_PATH_SUBATTR = 'subattr'
+KEY_PATH_SUBSUBTAG = 'subsubtag'
+
 
 path = []
 rule = []
 
+root = {'debugLevel':'0', 'integratorAbsoluteTolerance': '1.0E-6' , 'integratorRelativeTolerance' : '1.0E-6' , 'toleranceOnTime' : '1.0E-10', 'maxIntegrationTimeInterval' : '100001.0' , 'maximumStepSize' : '0.0' , 'realTimeScaling' : '0.0' , 'solver' : '1.0', 'gridEnabled' : '1' }
 
 path.append(
     {
-        'tag': 'ScilabDouble',
-        'attr': 'as',
-        'attrvalue': 'nmode',
-        'subtag':'data',
-        'subattr':'realPart'
+        KEY_PATH_TAG: 'ScilabDouble',
+        KEY_PATH_ATTR: 'as',
+        KEY_PATH_ATTRVALUE: 'nmode',
+        KEY_PATH_SUBTAG:'data',
+        KEY_PATH_SUBATTR:'realPart'
     })
 rule.append({
-    'op':DOUBLE_TO_INTEGER,
-    'tag':'ScilabInteger',
-    'attribute':{'intPrecision':'sci_int32'},
-    'attr':'value'
+    KEY_RULE_OP : DOUBLE_TO_INTEGER,
+    KEY_RULE_TAG : 'ScilabInteger',
+    KEY_RULE_ATTRIBUTE:{'intPrecision':'sci_int32'},
+    KEY_RULE_ATTR:'value'
 })
 
 
 
 path.append(    
     {
-        'tag': 'ScilabDouble',
-        'attr': 'as',
-        'attrvalue': 'nbZerosCrossing',
-        'subtag':'data',
-        'subattr':'realPart'
+        KEY_PATH_TAG: 'ScilabDouble',
+        KEY_RULE_ATTR : 'as',
+        KEY_PATH_ATTRVALUE : 'nbZerosCrossing',
+        KEY_PATH_SUBTAG : 'data',
+        KEY_PATH_SUBATTR : 'realPart'
     })
 rule.append({
-    'op':DOUBLE_TO_INTEGER,
-    'tag':'ScilabInteger',
-    'attribute':{'intPrecision':'sci_int32'},
-    'attr':'value'
+    KEY_RULE_OP : DOUBLE_TO_INTEGER,
+    KEY_RULE_TAG : 'ScilabInteger',
+    KEY_RULE_ATTRIBUTE:{'intPrecision':'sci_int32'},
+    KEY_RULE_ATTR:'value'
 })
 
 
 
 path.append(
     {
-        'tag':'BasicBlock',
+        KEY_PATH_TAG :'BasicBlock',
         
     })
 rule.append({
-    'op':DELETE_ATTRIB,
-    'attr':'ordering'
+    KEY_RULE_OP : DELETE_ATTRIB,
+    KEY_RULE_ATTR : 'ordering'
 })
 
 path.append(
     {
-        'tag':'BasicBlock',
+        KEY_PATH_TAG : 'BasicBlock',
         
     })
 rule.append({
-    'op':MAIN_BLOCK,
-    'tag':'ScilabDouble',
-    'attr':{'as':'state','height':'0','width':'0'},
-    'attribute':{'dependsOnT' : '0'},
-    'attr1' : 'dependsOnU',
-    'value' : '0'
+    KEY_RULE_OP : MAIN_BLOCK,
+    KEY_RULE_TAG : 'ScilabDouble',
+    KEY_RULE_ATTR : {'as':'state','height':'0','width':'0'},
+    KEY_RULE_ATTRIBUTE : {'dependsOnT' : '0'},
+    KEY_RULE_ATTR1 : 'dependsOnU',
+    KEY_RULE_VALUE : '0'
 })
 
 path.append(
     {
-        'tag':'BasicBlock',
+        KEY_PATH_TAG :'BasicBlock',
         
     })
 rule.append({
-    'op':MAIN_BLOCK,
-    'tag':'ScilabDouble',
-    'attr':{'as':'dState','height':'0','width':'0'},
-    'attribute':{'dependsOnT' : '0'},
-    'attr1' : 'dependsOnU',
-    'value' : '0'
+    KEY_RULE_OP : MAIN_BLOCK,
+    KEY_RULE_TAG : 'ScilabDouble',
+    KEY_RULE_ATTR : {'as':'dState','height':'0','width':'0'},
+    KEY_RULE_ATTRIBUTE : {'dependsOnT' : '0'},
+    KEY_RULE_ATTR1 : 'dependsOnU',
+    KEY_RULE_VALUE : '0'
 })
 
 
 
 path.append(
     {
-        'tag':'Product',
+        KEY_PATH_TAG :'Product',
         
     })
 rule.append({
-    'op':DELETE_ATTRIB,
-    'attr':'ordering'
+    KEY_RULE_OP : DELETE_ATTRIB,
+    KEY_RULE_ATTR : 'ordering'
 })
 
 path.append(
     {
-        'tag':'Product',
+        KEY_PATH_TAG :'Product',
         
     })
 rule.append({
-    'op':MAIN_BLOCK,
-    'tag':'ScilabDouble',
-    'attr':{'as':'state','height':'0','width':'0'},
-    'attribute':{'dependsOnT' : '0'},
-    'attr1' : 'dependsOnU',
-    'value' : '0'
+    KEY_RULE_OP : MAIN_BLOCK,
+    KEY_RULE_TAG : 'ScilabDouble',
+    KEY_RULE_ATTR : {'as':'state','height':'0','width':'0'},
+    KEY_RULE_ATTRIBUTE : {'dependsOnT' : '0'},
+    KEY_RULE_ATTR1 : 'dependsOnU',
+    KEY_RULE_VALUE : '0'
 })
 
 path.append(
     {
-        'tag':'Product',
+        KEY_PATH_TAG :'Product',
         
     })
 rule.append({
-    'op':MAIN_BLOCK,
-    'tag':'ScilabDouble',
-    'attr':{'as':'dState','height':'0','width':'0'},
-    'attribute':{'dependsOnT' : '0'},
-    'attr1' : 'dependsOnU',
-    'value' : '0'
+    KEY_RULE_OP : MAIN_BLOCK,
+    KEY_RULE_TAG : 'ScilabDouble',
+    KEY_RULE_ATTR : {'as':'dState','height':'0','width':'0'},
+    KEY_RULE_ATTRIBUTE:{'dependsOnT' : '0'},
+    KEY_RULE_ATTR1 : 'dependsOnU',
+    KEY_RULE_VALUE : '0'
 })
 
 
 path.append(
     {
-        'tag':'BigSom',
+        KEY_PATH_TAG :'BigSom',
         
     })
 rule.append({
-    'op':DELETE_ATTRIB,
-    'attr':'ordering',
+    KEY_RULE_OP : DELETE_ATTRIB,
+    KEY_RULE_ATTR : 'ordering',
 })
 
 path.append(
     {
-        'tag':'BigSom',
+        KEY_PATH_TAG :'BigSom',
         
     })
 rule.append({
-    'op':MAIN_BLOCK,
-    'tag':'ScilabDouble',
-    'attr':{'as':'state','height':'0','width':'0'},
-    'attribute':{'dependsOnT' : '0'},
-    'attr1' : 'dependsOnU',
-    'value' : '0'
+    KEY_RULE_OP : MAIN_BLOCK,
+    KEY_RULE_TAG : 'ScilabDouble',
+    KEY_RULE_ATTR : {'as':'state','height':'0','width':'0'},
+    KEY_RULE_ATTRIBUTE : {'dependsOnT' : '0'},
+    KEY_RULE_ATTR1 : 'dependsOnU',
+    KEY_RULE_VALUE : '0'
 })
 
 path.append(
     {
-        'tag':'BigSom',
+        KEY_PATH_TAG :'BigSom',
         
     })
 rule.append({
-    'op':MAIN_BLOCK,
-    'tag':'ScilabDouble',
-    'attr':{'as':'dState','height':'0','width':'0'},
-    'attribute':{'dependsOnT' : '0'},
-    'attr1' : 'dependsOnU',
-    'value' : '0'
+    KEY_RULE_OP : MAIN_BLOCK,
+    KEY_RULE_TAG:'ScilabDouble',
+    KEY_RULE_ATTR :{'as':'dState','height':'0','width':'0'},
+    KEY_RULE_ATTRIBUTE:{'dependsOnT' : '0'},
+    KEY_RULE_ATTR1 : 'dependsOnU',
+    KEY_RULE_VALUE : '0'
 })
 
 
 
 path.append(
     {
-        'tag':'AfficheBlock',
+        KEY_PATH_TAG :'AfficheBlock',
         
     }
 )
 rule.append({
-    'op':DELETE_ATTRIB,
-    'attr':'ordering',
+    KEY_RULE_OP:DELETE_ATTRIB,
+    KEY_RULE_ATTR:'ordering',
 })
 path.append(
     {
-        'tag':'AfficheBlock',
+        KEY_PATH_TAG :'AfficheBlock',
         
     }
 )
 rule.append({
-    'op':MAIN_BLOCK,
-    'tag':'ScilabDouble',
-    'attr':{'as':'state','height':'0','width':'0'},
-    'attribute':{'dependsOnT' : '0'},
-    'attr1' : 'dependsOnU',
-    'value' : '0'
+    KEY_RULE_OP : MAIN_BLOCK,
+    KEY_RULE_TAG :'ScilabDouble',
+    KEY_RULE_ATTR : {'as':'state','height':'0','width':'0'},
+    KEY_RULE_ATTRIBUTE : {'dependsOnT' : '0'},
+    KEY_RULE_ATTR1 : 'dependsOnU',
+    KEY_RULE_VALUE : '0'
 })
 
 path.append(
     {
-        'tag':'AfficheBlock',
+        KEY_PATH_TAG :'AfficheBlock',
         
     }
 )
 rule.append({
-    'op':MAIN_BLOCK,
-    'tag':'ScilabDouble',
-    'attr':{'as':'dState','height':'0','width':'0'},
-    'attribute':{'dependsOnT' : '0'},
-    'attr1' : 'dependsOnU',
-    'value' : '0'
+    KEY_RULE_OP:MAIN_BLOCK,
+    KEY_RULE_TAG:'ScilabDouble',
+    KEY_RULE_ATTR:{'as':'dState','height':'0','width':'0'},
+    KEY_RULE_ATTRIBUTE:{'dependsOnT' : '0'},
+    KEY_RULE_ATTR1 : 'dependsOnU',
+    KEY_RULE_VALUE : '0'
 })
 
 
 
 path.append(
     {
-        'tag':'ExplicitLink',
-        'subtag':'mxGeometry',
+        KEY_PATH_TAG :'ExplicitLink',
+        KEY_PATH_SUBTAG :'mxGeometry',
     }
 )
 rule.append({
-    'op':DELETE_SUB_ATTRIB,
-    'attr':'y',
+    KEY_RULE_OP:DELETE_SUB_ATTRIB,
+    KEY_RULE_ATTR:'y',
 })
 
 path.append(
     {
-        'tag':'ExplicitLink',
-        'subtag':'mxGeometry',
+        KEY_PATH_TAG :'ExplicitLink',
+        KEY_PATH_SUBTAG :'mxGeometry',
  
     }
 )
 rule.append({
-    'op':ADD_SUB_SUBTAG,
-    'subsubtag':'mxPoint',
-    'attr':{'as':'sourcePoint','x':'0.0','y':'0.0'},
-    'subsubtag1':'mxPoint',
-    'attr1':{'as':'targetPoint','x':'0.0','y':'0.0'}
+    KEY_RULE_OP:ADD_SUB_SUBTAG,
+    KEY_RULE_SUBSUBTAG:'mxPoint',
+    KEY_RULE_ATTR:{'as':'sourcePoint','x':'0.0','y':'0.0'},
+    KEY_RULE_SUBSUBTAG1:'mxPoint',
+    KEY_RULE_ATTR1:{'as':'targetPoint','x':'0.0','y':'0.0'}
 })
 
 
 path.append(
     {
-        'tag':'ExplicitLink',
-        'subtag':'mxGeometry',
-        'subsubtag':'Array',
+        KEY_PATH_TAG :'ExplicitLink',
+        KEY_PATH_SUBTAG :'mxGeometry',
+        KEY_PATH_SUBSUBTAG :'Array',
     }
 )
 rule.append({
-    'op':DELETE_SUBSUB_ATTRIB,
-    'attr':'scilabClass',
+    KEY_RULE_OP:DELETE_SUBSUB_ATTRIB,
+    KEY_RULE_ATTR:'scilabClass',
 })
 
 
 
 path.append(
     {
-        'tag':'ExplicitInputPort',
-        'subtag':'mxGeometry' ,
+        KEY_PATH_TAG :'ExplicitInputPort',
+        KEY_PATH_SUBTAG :'mxGeometry' ,
     })
 rule.append({
-    'op':DELETE_SUBTAG,
-    'subtag':'mxGeometry',
+    KEY_RULE_OP:DELETE_SUBTAG,
+    KEY_RULE_SUBTAG:'mxGeometry',
 })
 
 path.append(
     {
-        'tag':'ExplicitInputPort',
+        KEY_PATH_TAG :'ExplicitInputPort',
         
     })
 rule.append({
-    'op':ADD_ATTRIB,
-    'attribute':{'initialState':'-1.0'},
-    'attr' : 'dataLines',
-    'value' : "-1"
+    KEY_RULE_OP:ADD_ATTRIB,
+    KEY_RULE_ATTRIBUTE:{'initialState':'-1.0'},
+    KEY_RULE_ATTR : 'dataLines',
+    KEY_RULE_VALUE : "-1"
 })
 
 path.append(
     {
-        'tag':'ExplicitOutputPort',
-        'subtag':'mxGeometry' ,
+        KEY_PATH_TAG :'ExplicitOutputPort',
+        KEY_PATH_SUBTAG :'mxGeometry' ,
     })
 rule.append({
-    'op':DELETE_SUBTAG,
-    'subtag':'mxGeometry',
+    KEY_RULE_OP:DELETE_SUBTAG,
+    KEY_RULE_SUBTAG:'mxGeometry',
 })
 
 path.append(
     {
-        'tag':'ExplicitOutputPort',
+        KEY_PATH_TAG :'ExplicitOutputPort',
         
     })
 rule.append({
-    'op':ADD_ATTRIB,
-    'attribute':{'initialState':'-1.0'},
-    'attr' : 'dataLines',
-    'value' : "-1"
+    KEY_RULE_OP:ADD_ATTRIB,
+    KEY_RULE_ATTRIBUTE:{'initialState':'-1.0'},
+    KEY_RULE_ATTR : 'dataLines',
+    KEY_RULE_VALUE: "-1"
 })
 
 
 path.append(
     {
-        'tag': 'ScilabDouble',
-        'attr': 'as',
-        'attrvalue': 'integerParameters',
-        'subtag':'data',
-        'subattr':'realPart'
+        KEY_PATH_TAG : 'ScilabDouble',
+        KEY_PATH_ATTR : 'as',
+        KEY_PATH_ATTRVALUE : 'integerParameters',
+        KEY_PATH_SUBTAG:'data',
+        KEY_PATH_SUBATTR :'realPart'
     })
 rule.append({
-    'op':DOUBLE_TO_INTEGER_AND_SWAP,
-    'tag':'ScilabInteger',  
-    'attribute':{'intPrecision':'sci_int32'},
-    'attr':'value'
+    KEY_RULE_OP:DOUBLE_TO_INTEGER_AND_SWAP,
+    KEY_RULE_TAG:'ScilabInteger',  
+    KEY_RULE_ATTRIBUTE:{'intPrecision':'sci_int32'},
+    KEY_RULE_ATTR:'value'
 })
 
--- parser.py
+++ parser.py
@@ -9,14 +9,15 @@ root = tree.getroot()
 iter = root.getiterator()
 ch=root.getchildren()
 
+
 for i, path in enumerate(conf.path):
-    xpath = './/' + path['tag'] 
+    xpath = './/' + path[conf.KEY_PATH_TAG] 
     #if path.get('attr', None) is not None:
         #path.get('attrvalue', None) is not None
     if 'attr' in path:
-        xpath += '[@' + path['attr']
+        xpath += '[@' + path[conf.KEY_PATH_ATTR]
         if 'attrvalue' in path:
-            xpath += '=\'' + path['attrvalue'] + '\''
+            xpath += '=\'' + path[conf.KEY_PATH_ATTRVALUE] + '\''
         xpath += ']'    
 
     #print(xpath)
@@ -27,24 +28,24 @@ for i, path in enumerate(conf.path):
         
         #print(node.tag) 
 
-        if rl['op'] == conf.DOUBLE_TO_INTEGER :
-            node.tag = rl['tag']
-            for n ,v in rl['attribute'].items():
+        if rl[conf.KEY_RULE_OP] == conf.DOUBLE_TO_INTEGER :
+            node.tag = rl[conf.KEY_RULE_TAG]
+            for n ,v in rl[conf.KEY_RULE_ATTRIBUTE].items():
                 node.set(n,v)
             if 'subattr' in path:
-                x = './/'+ path['subtag'] + '[@' + path['subattr'] +']'
+                x = './/'+ path[conf.KEY_PATH_SUBTAG] + '[@' + path[conf.KEY_PATH_SUBATTR] +']'
                 m = node.findall(x)
                 for n in m:
-                    n.attrib[rl['attr']] =  str(int(float(n.attrib[path['subattr']])))
-                    del n.attrib[path['subattr']]
+                    n.attrib[rl[conf.KEY_RULE_ATTR]] =  str(int(float(n.attrib[path[conf.KEY_PATH_SUBATTR]])))
+                    del n.attrib[path[conf.KEY_PATH_SUBATTR]]
        
                 
-        elif rl['op'] == conf.DELETE_ATTRIB :                       #deleting attrib ordering
-            if rl['attr'] in node.attrib:
-                del node.attrib[rl['attr']]
+        elif rl[conf.KEY_RULE_OP] == conf.DELETE_ATTRIB :                       #deleting attrib ordering
+            if rl[conf.KEY_RULE_ATTR] in node.attrib:
+                del node.attrib[rl[conf.KEY_RULE_ATTR]]
 
 
-        elif rl['op'] == conf.MAIN_BLOCK:
+        elif rl[conf.KEY_RULE_OP] == conf.MAIN_BLOCK:
             a = SubElement(node,rl['tag'],rl['attr'])
             a.tail = '\n      '
             if rl['attr1'] not in node.attrib:          #add attrib dependsOnU if not present
@@ -52,61 +53,63 @@ for i, path in enumerate(conf.path):
             for n,v in rl['attribute'].items():         #add attrib dependsOnT
                 node.set(n,v)
 
-        elif rl['op'] == conf.ADD_SUB_SUBTAG :
-            x = './/'+ path['subtag']
+        elif rl[conf.KEY_RULE_OP] == conf.ADD_SUB_SUBTAG :
+            x = './/'+ path[conf.KEY_PATH_SUBTAG]
             n = node.find(x)                # finding mxgeometry
-            xp = './/'+ path['subtag'] +'['+ rl['subsubtag']+']'
+            xp = './/'+ path[conf.KEY_PATH_SUBTAG] +'['+ rl[conf.KEY_RULE_SUBSUBTAG]+']'
             np= node.find(xp)               # finding mx point tag in mxgeometry
             if np is None:
-                a = SubElement(n,rl['subsubtag'],rl['attr'])
+                a = SubElement(n,rl[conf.KEY_RULE_SUBSUBTAG],rl[conf.KEY_RULE_ATTR])
                 a.tail = "\n        "
-                b = SubElement(n,rl['subsubtag1'],rl['attr1'])
+                b = SubElement(n,rl[conf.KEY_RULE_SUBSUBTAG1],rl[conf.KEY_RULE_ATTR1])
                 b.tail = "\n        "       
 
 
-        elif rl['op'] == conf.DELETE_SUBTAG:            #removing mxgeometry tag
+        elif rl[conf.KEY_RULE_OP] == conf.DELETE_SUBTAG:            #removing mxgeometry tag
             if 'subtag' in path:
-                x = './/'+ path['subtag']
+                x = './/'+ path[conf.KEY_PATH_SUBTAG]
                 n = node.find(x)
                 node.remove(n)
             if len(node.getchildren()) == 0:
                 node.text=None
 
 
-        elif rl['op'] == conf.DOUBLE_TO_INTEGER_AND_SWAP:
+        elif rl[conf.KEY_RULE_OP] == conf.DOUBLE_TO_INTEGER_AND_SWAP:
             if int(node.attrib['height']) > 0:
-                node.tag = rl['tag']            #change tag
+                node.tag = rl[conf.KEY_RULE_TAG]            #change tag
                 node.attrib['height'],node.attrib['width'] = node.attrib['width'],node.attrib['height']     #exchange height & width
-                x = './/'+ path['subtag'] + '[@' + path['subattr'] +']'         #xpath for data
+                x = './/'+ path[conf.KEY_PATH_SUBTAG] + '[@' + path[conf.KEY_PATH_SUBATTR] +']'         #xpath for data
                 m = node.findall(x)
                 for n in m:
-                    n.attrib[rl['attr']] =  str(int(float(n.attrib[path['subattr']])))          #change to value
-                    del n.attrib[path['subattr']]
+                    n.attrib[rl[conf.KEY_RULE_ATTR]] =  str(int(float(n.attrib[path[conf.KEY_PATH_SUBATTR]])))          #change to value
+                    del n.attrib[path[conf.KEY_PATH_SUBATTR]]
                     n.attrib['column'],n.attrib['line'] = n.attrib['line'],n.attrib['column']   #exchange line & column
 
-                for n ,v in rl['attribute'].items():
+                for n ,v in rl[conf.KEY_RULE_ATTRIBUTE].items():
                     node.set(n,v)
 
 
-        elif rl['op'] == conf.DELETE_SUB_ATTRIB:                #delete subattrib 'y' from mxGeometry
-            x = './/'+ path['subtag']
+        elif rl[conf.KEY_RULE_OP] == conf.DELETE_SUB_ATTRIB:                #delete subattrib 'y' from mxGeometry
+            x = './/'+ path[conf.KEY_PATH_SUBTAG]
             n = node.find(x)
-            if rl['attr'] in n.attrib:
-                del n.attrib[rl['attr']]
+            if rl[conf.KEY_RULE_ATTR] in n.attrib:
+                del n.attrib[rl[conf.KEY_RULE_ATTR]]
 
 
-        elif rl['op'] == conf.DELETE_SUBSUB_ATTRIB:             #delete attrib scilabclass from array
-            x ='.//' + path['subsubtag']
+        elif rl[conf.KEY_RULE_OP] == conf.DELETE_SUBSUB_ATTRIB:             #delete attrib scilabclass from array
+            x ='.//' + path[conf.KEY_PATH_SUBSUBTAG]
             n = node.find(x)
-            del n.attrib[rl['attr']]
+            del n.attrib[rl[conf.KEY_RULE_ATTR]]
             
-        elif rl['op'] == conf.ADD_ATTRIB:
-            for k, v in rl['attribute'].items():                #add attrib initialport
+        elif rl[conf.KEY_RULE_OP] == conf.ADD_ATTRIB:
+            for k, v in rl[conf.KEY_RULE_ATTRIBUTE].items():                #add attrib initialport
                 node.set(k,v)
-            if rl['attr'] not in node.attrib:                   # if attrib 'datalines' is not present add
-                node.set(rl['attr'],rl['value'])
-        
+            if rl[conf.KEY_RULE_ATTR] not in node.attrib:                   # if attrib 'datalines' is not present add
+                node.set(rl[conf.KEY_RULE_ATTR],rl[conf.KEY_RULE_VALUE])
 
+        
+for n,v in conf.root.items():
+    root.set(n,v)
 root.text= None
 root.text = "\n  "
 comment = Comment("Xcos - 2.0 - scilab-6.0.2 - 20190911")
